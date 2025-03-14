7437f68291c50536c2385011cfd00ead7dce20a3
LiuDehao <liu.dehao@outlook.com>
2025-03-14 21:02:24 +0800

    style: 忘删调试代码了，另外一些小技巧让代码变短

--- 221.go
+++ 221.go
@@ -1,60 +1,25 @@
 package main
 
-import "fmt"
-
 func maximalSquare(matrix [][]byte) int {
-	m := len(matrix)
-	n := len(matrix[0])
+	m, n := len(matrix), len(matrix[0])
 	ans := 0
-	dp := [300][300]int{}
+	dp := make([][]int, m)
 	for i := 0; i < m; i++ {
-		if matrix[i][0] == '1' {
-			dp[i][0] = 1
-			ans = 1
-		}
-	}
-	for j := 0; j < n; j++ {
-		if matrix[0][j] == '1' {
-			dp[0][j] = 1
-			ans = 1
+		dp[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			dp[i][j] = int(matrix[i][j] - '0')
+			if dp[i][j] == 1 {
+				ans = 1
+			}
 		}
 	}
 	for i := 1; i < m; i++ {
 		for j := 1; j < n; j++ {
-			if matrix[i][j] == '0' {
-				dp[i][j] = 0
-				continue
+			if dp[i][j] == 1 {
+				dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+				ans = max(ans, dp[i][j])
 			}
-			lu := dp[i-1][j-1]
-			u, l := dp[i-1][j], dp[i][j-1]
-			if u >= lu && l >= lu {
-				dp[i][j] = lu + 1
-			} else {
-				dp[i][j] = min(u, l) + 1
-			}
-			ans = max(ans, dp[i][j])
 		}
-	}
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			fmt.Printf("%d ", dp[i][j])
-		}
-		fmt.Println()
 	}
 	return ans * ans
-}
-
-func main() {
-	n := 300
-	a := make([][]byte, 300)
-	for i := 0; i < n; i++ {
-		a[i] = make([]byte, 300)
-	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			a[i][j] = '1'
-		}
-	}
-	maximalSquare(a)
-
 }
