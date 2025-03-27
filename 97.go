08cde39ed20015ab84d1631dcab445307ef09066
LiuDehao <liu.dehao@outlook.com>
2025-03-27 21:05:18 +0800

    style: 边界条件可以放在双重循环里，为滚动数组优化做准备

--- 97.go
+++ 97.go
@@ -11,16 +11,15 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
 		dp[i] = make([]bool, n2+1)
 	}
 	dp[0][0] = true
-	for i := 1; i <= n1; i++ {
-		dp[i][0] = dp[i-1][0] && s1[i-1] == s3[i-1]
-	}
-	for j := 1; j <= n2; j++ {
-		dp[0][j] = dp[0][j-1] && s2[j-1] == s3[j-1]
-	}
-	for i := 1; i <= n1; i++ {
-		for j := 1; j <= n2; j++ {
-			dp[i][j] = dp[i][j] || (dp[i-1][j] && s1[i-1] == s3[i+j-1])
-			dp[i][j] = dp[i][j] || (dp[i][j-1] && s2[j-1] == s3[i+j-1])
+	for i := 0; i <= n1; i++ {
+		for j := 0; j <= n2; j++ {
+			p := i + j - 1
+			if i > 0 {
+				dp[i][j] = dp[i][j] || (dp[i-1][j] && s1[i-1] == s3[p])
+			}
+			if j > 0 {
+				dp[i][j] = dp[i][j] || (dp[i][j-1] && s2[j-1] == s3[p])
+			}
 		}
 	}
 	return dp[n1][n2]
