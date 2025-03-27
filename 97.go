b5320ba65911c2c4cbed032d4972c4825e11c721
LiuDehao <liu.dehao@outlook.com>
2025-03-27 21:08:40 +0800

    insight: 滚动数组优化，我还不会，做几道试试.

--- 97.go
+++ 97.go
@@ -5,22 +5,19 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
 	if n1+n2 != n3 {
 		return false
 	}
-	// 定义f(i,j)表示s1[:i]与s2[:j]可以交错组成s3[:i+j]
-	dp := make([][]bool, n1+1)
-	for i := range dp {
-		dp[i] = make([]bool, n2+1)
-	}
-	dp[0][0] = true
+	// 滚动数组优化
+	dp := make([]bool, n2+1)
+	dp[0] = true
 	for i := 0; i <= n1; i++ {
 		for j := 0; j <= n2; j++ {
 			p := i + j - 1
 			if i > 0 {
-				dp[i][j] = dp[i][j] || (dp[i-1][j] && s1[i-1] == s3[p])
+				dp[j] = dp[j] && s1[i-1] == s3[p]
 			}
 			if j > 0 {
-				dp[i][j] = dp[i][j] || (dp[i][j-1] && s2[j-1] == s3[p])
+				dp[j] = dp[j] || dp[j-1] && s2[j-1] == s3[p]
 			}
 		}
 	}
-	return dp[n1][n2]
+	return dp[n2]
 }
