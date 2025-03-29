05d213662a9085d695adfa708c54e3eed4ab7239
LiuDehao <liu.dehao@outlook.com>
2025-03-29 11:49:32 +0800

    insight: 学到了螺旋的好方法！

--- 54.go
+++ 54.go
@@ -3,48 +3,39 @@ package main
 import "fmt"
 
 func spiralOrder(matrix [][]int) []int {
-	if len(matrix) == 0 {
-		return nil
-	}
-	m, n := len(matrix), len(matrix[0])
-	if m == 1 {
-		return matrix[0]
-	} else if n == 1 {
-		ans := make([]int, m)
-		for i := range m {
-			ans[i] = matrix[i][0]
+	l, r, t, b := 0, len(matrix[0])-1, 0, len(matrix)-1
+	var ans []int
+	for {
+		for i := l; i <= r; i++ {
+			ans = append(ans, matrix[t][i])
 		}
-		return ans
-	}
-
-	ans := make([]int, (n+m-2)*2)
-	p := 0
-
-	for j := range n - 1 {
-		ans[p] = matrix[0][j]
-		p++
-	}
-	for i := range m - 1 {
-		ans[p] = matrix[i][n-1]
-		p++
-	}
-	for j := range n - 1 {
-		ans[p] = matrix[m-1][n-1-j]
-		p++
-	}
-	for i := range m - 1 {
-		ans[p] = matrix[m-1-i][0]
-		p++
-	}
-
-	innerMatrix := make([][]int, m-2)
-	for i := range innerMatrix {
-		innerMatrix[i] = make([]int, n-2)
-		for j := range innerMatrix[i] {
-			innerMatrix[i][j] = matrix[i+1][j+1]
+		t++
+		if t > b {
+			break
+		}
+		for i := t; i <= b; i++ {
+			ans = append(ans, matrix[i][r])
+		}
+		r--
+		if r < l {
+			break
+		}
+		for i := r; i >= l; i-- {
+			ans = append(ans, matrix[b][i])
+		}
+		b--
+		if b < t {
+			break
+		}
+		for i := b; i >= t; i-- {
+			ans = append(ans, matrix[i][l])
+		}
+		l++
+		if l > r {
+			break
 		}
 	}
-	return append(ans, spiralOrder(innerMatrix)...)
+	return ans
 }
 
 func main() {
