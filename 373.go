8dabb9bbe7ae8f405549ca1627705f33aad7f791
LiuDehao <liu.dehao@outlook.com>
2025-04-07 23:09:24 +0800

    insight: 这次真的熟悉heap最合适的写法了，加油

--- 373.go
+++ 373.go
@@ -3,55 +3,56 @@ package main
 import "container/heap"
 
 type Pair struct {
-	i, j int
-	x, y int
+	i, j int // 只需要存索引！
 }
 
-type PairSlice struct {
-	pairs []Pair
+type PairSlice struct { // 其实命名hp， 然后func (h hp) ... 合适吧
+	nums1, nums2 []int // 就可以通过索引获取数据哩！
+	data         []Pair
 }
 
 func (ps PairSlice) Len() int {
-	return len(ps.pairs)
+	return len(ps.data)
 }
 
 func (ps PairSlice) Less(i, j int) bool {
-	p1, p2 := ps.pairs[i], ps.pairs[j]
-	return p1.x+p1.y < p2.x+p2.y
+	p1, p2 := ps.data[i], ps.data[j]
+	return ps.nums1[p1.i]+ps.nums2[p1.j] < ps.nums1[p2.i]+ps.nums2[p2.j]
 }
 
 func (ps PairSlice) Swap(i, j int) {
-	tmp := ps.pairs[i]
-	ps.pairs[i] = ps.pairs[j]
-	ps.pairs[j] = tmp
+	// go语言是支持这个的...
+	ps.data[i], ps.data[j] = ps.data[j], ps.data[i]
 }
 
-func (ps *PairSlice) Push(x any) {
-	ps.pairs = append(ps.pairs, x.(Pair))
+func (ps *PairSlice) Push(v any) {
+	ps.data = append(ps.data, v.(Pair))
 }
 
 func (ps *PairSlice) Pop() any {
-	a := ps.pairs
+	a := ps.data
 	n := ps.Len()
 	last := a[n-1]
-	ps.pairs = a[:n-1]
+	ps.data = a[:n-1]
 	return last
 }
 
 func kSmallestPairs(nums1 []int, nums2 []int, k int) [][]int {
 	n1, n2 := len(nums1), len(nums2)
-	ps := &PairSlice{make([]Pair, n1)}
+	ps := &PairSlice{nums1, nums2, make([]Pair, n1)}
 
 	for i := range n1 {
-		ps.pairs[i] = Pair{i, 0, nums1[i], nums2[0]}
+		ps.data[i] = Pair{i, 0}
 	}
+
 	var ans [][]int
 	heap.Init(ps)
 	for _ = range k {
 		top := heap.Pop(ps).(Pair)
-		ans = append(ans, []int{top.x, top.y})
-		if top.j+1 < n2 {
-			heap.Push(ps, Pair{top.i, top.j + 1, nums1[top.i], nums2[top.j+1]})
+		i, j := top.i, top.j
+		ans = append(ans, []int{nums1[i], nums2[j]})
+		if j+1 < n2 {
+			heap.Push(ps, Pair{i, j + 1})
 		}
 	}
 	return ans
