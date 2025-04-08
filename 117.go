cf0651695ebe4837deb60ed20e74415233d210b3
LiuDehao <liu.dehao@outlook.com>
2025-04-08 21:22:03 +0800

    insight: 发现了层序遍历的好写法！

---
+++ 117.go
@@ -0,0 +1,32 @@
+package main
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+	q := []*Node{root}
+	for len(q) > 0 {
+		// 这个写法好好啊，我要学会
+		tmp := q
+		q = nil
+		for i, node := range tmp {
+			if i+1 < len(tmp) {
+				node.Next = tmp[i+1]
+			}
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+	}
+	return root
+}
