5653971e2ae2604c56ce8e7da636e7726e66028a
LiuDehao <liu.dehao@outlook.com>
2025-04-08 21:01:47 +0800

    insight: 数组可以map

--- 49.go
+++ 49.go
@@ -1,23 +1,15 @@
 package main
 
 func groupAnagrams(strs []string) [][]string {
-	ansMap := make(map[string][]string)
+	ansMap := make(map[[26]int][]string)
 	for _, str := range strs {
-		chs := make([]int, 26)
-		for i := range str {
-			ch := str[i] - 'a'
-			chs[ch]++
+		chs := [26]int{}
+		for _, v := range str {
+			chs[v]++
 		}
-		var bs []byte
-		for i, v := range chs {
-			for ; v > 0; v-- {
-				bs = append(bs, uint8(i)+'a')
-			}
-		}
-		s := string(bs)
-		ansMap[s] = append(ansMap[s], str)
+		ansMap[chs] = append(ansMap[chs], str)
 	}
-	var ans [][]string
+	ans := make([][]string, 0, len(ansMap))
 	for _, v := range ansMap {
 		ans = append(ans, v)
 	}
