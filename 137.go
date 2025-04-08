603a484fdf6f25ce3026523268632c9c830e32dd
LiuDehao <liu.dehao@outlook.com>
2025-04-08 20:09:04 +0800

    cheat: 没想到这个题还能用数电的思路，但我不做。

--- 137.go
+++ 137.go
@@ -1,16 +1,11 @@
 package main
 
 func singleNumber(nums []int) int {
-	// 答案的第i个二进制位就是数组中所有元素的第i个二进制位之和除以3的余数
-	ans := int32(0)
-	for i := 0; i < 32; i++ {
-		total := int32(0)
-		for _, num := range nums {
-			total += int32(num) >> i & 1
-		}
-		if total%3 > 0 {
-			ans |= 1 << i
-		}
+	a, b := 0, 0
+	for _, num := range nums {
+		// 以下优化自 a, b = b&^a&num|a&^b&^num, (b^num)&^a
+		b = (b ^ num) &^ a
+		a = (a ^ num) &^ b
 	}
-	return int(ans)
+	return b
 }
