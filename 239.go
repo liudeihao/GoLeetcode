29bd102d514d9dfb6685e51ef99faf742c37cfa5
LiuDehao <liu.dehao@outlook.com>
2025-07-17 13:47:06 +0800

    239. 滑动窗口最大值. 这是单调队列！

--- 239.go
+++ 239.go
@@ -1,35 +1,45 @@
 package main
 
+// MaxQueue 单调队列
+type MaxQueue struct {
+	queue []int
+}
+
+func (q *MaxQueue) Front() int {
+	return q.queue[0]
+}
+func (q *MaxQueue) Back() int {
+	return q.queue[len(q.queue)-1]
+}
+func (q *MaxQueue) Empty() bool {
+	return len(q.queue) == 0
+}
+func (q *MaxQueue) Push(val int) {
+	for !q.Empty() && val > q.Back() { // 这里是val > q.Back()而不是 >=, 不然Pop就坏了
+		// pop_back，维护单调队列，这里区别于一般队列
+		q.queue = q.queue[:len(q.queue)-1]
+	}
+	q.queue = append(q.queue, val)
+}
+func (q *MaxQueue) Pop(val int) {
+	// 这里判断是因为它可能已经被pop_back了！
+	if !q.Empty() && val == q.Front() {
+		q.queue = q.queue[1:]
+	}
+}
+
 func maxSlidingWindow(nums []int, k int) []int {
-	deque := []int{0}
-	push := func(x int) {
-		deque = append(deque, x)
+	q := MaxQueue{}
+	var ans []int
+	for i := 0; i < k; i++ {
+		q.Push(nums[i])
 	}
-	top := func() int {
-		return deque[len(deque)-1]
-	}
-	pop_back := func() {
-		deque = deque[:len(deque)-1]
-	}
-	empty := func() bool {
-		return len(deque) == 0
-	}
-	for i := 1; i < k; i++ {
-		for !empty() && nums[top()] <= nums[i] {
-			pop_back()
-		}
-		push(i)
-	}
-	ans := []int{nums[deque[0]]}
+	ans = append(ans, q.Front())
+
 	for i := k; i < len(nums); i++ {
-		for !empty() && (top() < i-k+1 || nums[top()] <= nums[i]) {
-			pop_back()
-		}
-		push(i)
-		for deque[0] < i-k+1 {
-			deque = deque[1:]
-		}
-		ans = append(ans, nums[deque[0]])
+		q.Pop(nums[i-k])
+		q.Push(nums[i])
+		ans = append(ans, q.Front())
 	}
 	return ans
 }
