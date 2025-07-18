3e22495f1a2e16c4f98ac147d277eb73307cf65b
LiuDehao <liu.dehao@outlook.com>
2025-07-18 18:40:06 +0800

    40.组合总和II. 学会剪枝去重了，这是理解之后的更好的写法

--- 40.go
+++ 40.go
@@ -9,9 +9,6 @@ func combinationSum2(nums []int, target int) [][]int {
 	var path []int
 	var sum int
 
-	// 要去重的是同一层（兄弟间）的“使用过”，同一树枝上的都是一个组合里的元素，不用去重
-	used := make([]bool, len(nums))
-
 	var backtrack func(idx int)
 	backtrack = func(idx int) {
 		if sum == target {
@@ -21,16 +18,14 @@ func combinationSum2(nums []int, target int) [][]int {
 			return
 		}
 		for j := idx; j < len(nums) && nums[j]+sum <= target; j++ {
-			if j > 0 && nums[j] == nums[j-1] && used[j-1] == false {
-				// used[j-1] == false说明左兄弟用了nums[j-1]，去除。
-				// used[j-1] == true说明父亲用了nums[j-1]，保留。
+			if j > idx && nums[j] == nums[j-1] {
+				// 只需要第一个idx就够了，它后面会遍历到后面的。
+				// 多想想
 				continue
 			}
 			path = append(path, nums[j])
 			sum += nums[j]
-			used[j] = true
 			backtrack(j + 1)
-			used[j] = false
 			sum -= nums[j]
 			path = path[:len(path)-1]
 		}
