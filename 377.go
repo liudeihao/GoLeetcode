56526877aceca894ffed16eb1e286af5183e475c
LiuDehao <liu.dehao@outlook.com>
2025-07-26 11:36:07 +0800

    377. 组合总和 Ⅳ. 如果用完全背包的思想做排列，则外层循环为背包容量（无法理解）

---
+++ 377.go
@@ -0,0 +1,16 @@
+package main
+
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+	for j := 1; j <= target; j++ {
+		// 如果求组合数就是外层for循环遍历物品，内层for遍历背包。
+		// 如果求排列数就是外层for遍历背包，内层for循环遍历物品。
+		for _, v := range nums {
+			if j >= v {
+				dp[j] += dp[j-v]
+			}
+		}
+	}
+	return dp[target]
+}
