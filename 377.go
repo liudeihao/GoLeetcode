8d11e0db7b8c3f95ba6de1704f10296c8a844b5d
LiuDehao <liu.dehao@outlook.com>
2025-07-26 12:22:18 +0800

    377. 组合总和 Ⅳ. 其实就是爬楼梯，而代码完全相同

--- 377.go
+++ 377.go
@@ -3,6 +3,21 @@ package main
 func combinationSum4(nums []int, target int) int {
 	dp := make([]int, target+1)
 	dp[0] = 1
+	// 爬楼梯，nums是可能的跳跃距离
+	for j := 1; j <= target; j++ {
+		for _, v := range nums {
+			if v <= j {
+				dp[j] += dp[j-v]
+			}
+		}
+	}
+	return dp[target]
+}
+
+/*
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
 	for j := 1; j <= target; j++ {
 		// 如果求组合数就是外层for循环遍历物品，内层for遍历背包。
 		// 如果求排列数就是外层for遍历背包，内层for循环遍历物品。
@@ -14,3 +29,4 @@ func combinationSum4(nums []int, target int) int {
 	}
 	return dp[target]
 }
+*/
